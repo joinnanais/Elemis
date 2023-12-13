78f068e237ee42dde74d6adfdc81a674a3ccd152
Joinna Anais <81961351+joinnanais@users.noreply.github.com>
2023-12-13 14:15:43 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,35 +1,52 @@
 import streamlit as st
+from streamlit.logger import get_logger
 import pandas as pd
-import datetime
+import plotly as px
+from streamlit.hello.utils import show_code
+import altair as alt
 
-# Set up date variables 
-
-current_date = datetime.datetime.now()
-next_year = current_date.year + 1
-jan_1 = datetime.date(next_year, 1, 1)
-dec_31 = datetime.date(next_year, 12, 31)
+LOGGER = get_logger(__name__)
 
 
-# Set up the landing page
-st.set_page_config(
-    page_title= "Sales Dashboard",
-    page_icon= ":bar_chart:",
-    layout = "wide"
-)
+def run():
+    st.set_page_config(
+        page_title="Sales Dashboard",
+        page_icon=":bar_chart:",
+    )
 
-#Create a file path
-csv_file_path = 'https://github.com/joinnanais/Elemis/blob/main/Sample%20-%20Superstore.csv'
+    st.write("# Sales Dashboard :bar_chart:")
 
-#Read csv file and select encoding 
-data = pd.read_csv(csv_file_path, encoding='latin1')
+    st.sidebar.success("Select a demo above.")
 
-df = pd.DataFrame(data)
+    st.markdown(
+        """
+        This is a prototype dashboard to demonstrate the concept of Streamlit for CDP purposes.
+        
 
-#Print dataframe
-print(df)
+        The data is from an open-source.
+        
+        
+        The data used is a sample data from Kaggle.
+        """
+    )
 
-# Assuming df is your DataFrame
+if __name__ == "__main__":
+    run()
 
-category_filter = st.selectbox("Filter by Category", pd.unique(df["Category"]))
-subcategory_filter = st.selectbox("Filter by Sub-Category", pd.unique(df["Sub-Category"]))
+github_csv_url = 'https://raw.githubusercontent.com/joinnanais/Elemis/main/Sample%20-%20Superstore.csv'
+df = pd.read_csv(github_csv_url, encoding='latin1')
 
+st.sidebar.header("Filters")
+
+# Select category filter
+category_filter = st.sidebar.multiselect("Select Category", df['Category'].unique(), df['Category'].unique())
+
+# Select sub-category filter
+sub_category_filter = st.sidebar.multiselect("Select Sub-Category", df['Sub-Category'].unique(), df['Sub-Category'].unique())
+
+filtered_df = df[(df['Category'].isin(category_filter)) & 
+                 (df['Sub-Category'].isin(sub_category_filter))]
+
+# Display the filtered dataframe
+st.subheader("Filtered Data")
+st.write(filtered_df)
